fd14b4364e0b8b660e04a252d5a6b2fe15f38f19
mazinod13 <chasmuempire28@gmail.com>
2026-08-31 17:20:54 +0545

    feat(warehouse): created dimensions and updated values into dim.employee

---
+++ warehouse/04_load_dim_employee.sql
@@ -0,0 +1,39 @@
+
+
+
+DELETE FROM dw.dim_employee;
+
+
+INSERT INTO dw.dim_employee(
+    employee_id,
+    full_name,
+    email,
+    hire_date,
+    manager_name,
+    store_name
+)
+SELECT
+    e.employee_id,
+    e.first_name || ' ' || e.last_name AS full_name,
+    e.email,
+    e.hire_date,
+    mgr.first_name || ' ' || mgr.last_name AS manager_name,
+    s.name AS store_name
+FROM public.employees as e
+LEFT JOIN public.employees as mgr
+    ON mgr.employee_id = e.manager_id
+LEFT JOIN public.stores as s
+    ON s.store_id = e.store_id
+ON CONFLICT(employee_id)
+DO UPDATE SET
+    full_name =  EXCLUDED.full_name,
+    email = EXCLUDED.email,
+    hire_date = EXCLUDED.hire_date,
+    manager_name = EXCLUDED.manager_name,
+    store_name = EXCLUDED.store_name
+WHERE
+    dw.dim_employee.full_name IS DISTINCT FROM EXCLUDED.full_name            
+    OR dw.dim_employee.email IS DISTINCT FROM EXCLUDED.email
+    OR dw.dim_employee.hire_date IS DISTINCT FROM EXCLUDED.hire_date
+    OR dw.dim_employee.manager_name IS DISTINCT FROM EXCLUDED.manager_name
+    OR dw.dim_employee.store_name IS DISTINCT FROM EXCLUDED.store_name;
